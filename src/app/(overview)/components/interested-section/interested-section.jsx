f31e7a78ed66bb79716f282cff4cf2f2cbf2c7c2
Stasia <stasia@Stasias-MacBook.local>
2024-03-28 19:02:10 +0200

    yo yo valery

--- src/app/(overview)/components/interested-section/interested-section.jsx
+++ src/app/(overview)/components/interested-section/interested-section.jsx
@@ -14,62 +14,62 @@ const InterestedSection = () => {
   const note4 = useRef(null);
   const ellipse = useRef(null);
 
-  useGSAP(() => {
-    gsap.to(note1.current, {
-      x: '-150%',
-      y: '-500%',
-      yoyo: true,
-      repeat: -1,
-      duration: 10,
-    });
+  // useGSAP(() => {
+  //   gsap.to(note1.current, {
+  //     x: '-150%',
+  //     y: '-500%',
+  //     yoyo: true,
+  //     repeat: -1,
+  //     duration: 10,
+  //   });
 
-    gsap.to(note2.current, {
-      x: '50%',
-      y: '-400%',
-      yoyo: true,
-      repeat: -1,
-      duration: 10,
-    });
+  //   gsap.to(note2.current, {
+  //     x: '50%',
+  //     y: '-400%',
+  //     yoyo: true,
+  //     repeat: -1,
+  //     duration: 10,
+  //   });
 
-    gsap.to(note3.current, {
-      x: '-550%',
-      y: '-50%',
-      yoyo: true,
-      repeat: -1,
-      duration: 10,
-    });
+  //   gsap.to(note3.current, {
+  //     x: '-550%',
+  //     y: '-50%',
+  //     yoyo: true,
+  //     repeat: -1,
+  //     duration: 10,
+  //   });
 
-    gsap.to(note4.current, {
-      x: '-40%',
-      y: '200%',
-      yoyo: true,
-      repeat: -1,
-      duration: 10,
-    });
+  //   gsap.to(note4.current, {
+  //     x: '-40%',
+  //     y: '200%',
+  //     yoyo: true,
+  //     repeat: -1,
+  //     duration: 10,
+  //   });
 
-    gsap.to('.ellipse', {
-      y: '-8rem',
-      yoyo: true,
-      repeat: -1,
-      duration: 10,
-    });
+  //   gsap.to('.ellipse', {
+  //     y: '-8rem',
+  //     yoyo: true,
+  //     repeat: -1,
+  //     duration: 10,
+  //   });
 
-    gsap.to('.ellipse_first', {
-      y: '-8rem',
-      opacity: 0.3,
-      yoyo: true,
-      repeat: -1,
-      duration: 10,
-    });
+  //   gsap.to('.ellipse_first', {
+  //     y: '-8rem',
+  //     opacity: 0.3,
+  //     yoyo: true,
+  //     repeat: -1,
+  //     duration: 10,
+  //   });
 
-    gsap.to('.ellipse_last', {
-      y: '-8rem',
-      opacity: 0.95,
-      yoyo: true,
-      repeat: -1,
-      duration: 10,
-    });
-  }, []);
+  //   gsap.to('.ellipse_last', {
+  //     y: '-8rem',
+  //     opacity: 0.95,
+  //     yoyo: true,
+  //     repeat: -1,
+  //     duration: 10,
+  //   });
+  // }, []);
 
   return (
     <section className={styles.interested_section}>
@@ -77,104 +77,39 @@ const InterestedSection = () => {
         {constants.INTERESTED_SECTION_TITLE}
       </p>
 
-      {/* <div className={styles.interested_section__book_button}>
+      <div className={styles.interested_section__book_button}>
         <BookADemo />
-      </div> */}
-
-      <div className={styles.interested_section__image_container}>
-        <Image
-          fill
-          src="/images/quantum-computer.svg"
-          alt="quantum computer"
-          className={styles.interested_section__image}
-        />
       </div>
 
-      <div
-        ref={ellipse}
-        className={`${styles.interested_section__elipse} ${styles.interested_section__elipse__1} ellipse_first`}
-      >
-        <Image
-          fill
-          src="/images/qc-ellipse.svg"
-          alt="elipse"
-        />
-      </div>
+      <div className={styles.interested_section__video_container}>
+        <video
+          width="100%"
+          height="100%"
+          loop
+          muted
+          autoPlay
+          webkit-playsinline="true"
+          playsInline
+          loading="lazy"
+        >
+          <source
+            src="/videos/quant-comp-desktop.mp4"
+            type="video/mp4"
+            media="(min-width: 834px)"
+          />
 
-      <div
-        ref={ellipse}
-        className={`${styles.interested_section__elipse} ${styles.interested_section__elipse__2} ellipse`}
-      >
-        <Image
-          fill
-          src="/images/qc-ellipse.svg"
-          alt="elipse"
-        />
-      </div>
+          <source
+            media="(max-width: 430px)"
+            src="/videos/quant-comp-mobile.mp4"
+            type="video/mp4"
+          />
 
-      <div
-        ref={ellipse}
-        className={`${styles.interested_section__elipse} ${styles.interested_section__elipse__3} ellipse`}
-      >
-        <Image
-          fill
-          src="/images/qc-ellipse.svg"
-          alt="elipse"
-        />
-      </div>
-
-      <div
-        ref={ellipse}
-        className={`${styles.interested_section__elipse} ${styles.interested_section__elipse__4} ellipse_last`}
-      >
-        <Image
-          fill
-          src="/images/qc-ellipse.svg"
-          alt="elipse"
-        />
-      </div>
-
-      <div
-        className={`${styles.interested_section__note} ${styles.interested_section__note__1}`}
-        ref={note1}
-      >
-        <div className={styles.interested_section__circle} />
-        <p className={styles.interested_section__note_title}>
-          we’re
-          <br /> agnostic
-        </p>
-      </div>
-
-      <div
-        className={`${styles.interested_section__note} ${styles.interested_section__note__2}`}
-        ref={note2}
-      >
-        <div className={styles.interested_section__circle} />
-        <p className={styles.interested_section__note_title}>
-          4x Quantum <br />
-          Volume
-        </p>
-      </div>
-
-      <div
-        className={`${styles.interested_section__note} ${styles.interested_section__note__3}`}
-        ref={note3}
-      >
-        <div className={styles.interested_section__circle} />
-        <p className={styles.interested_section__note_title}>
-          Up to 20 <br />
-          qubits
-        </p>
-      </div>
-
-      <div
-        className={`${styles.interested_section__note} ${styles.interested_section__note__4}`}
-        ref={note4}
-      >
-        <div className={styles.interested_section__circle} />
-        <p className={styles.interested_section__note_title}>
-          &gt;100x deeper <br /> circuits
-        </p>
+          <source
+            media="(max-width: 834px)"
+            src="/videos/quant-comp-tablet.mp4"
+            type="video/mp4"
+          />
+        </video>
       </div>
     </section>
   );
